4b597e7379de53c17e8c55aec7344c32d0fd21ef
mikolas <mikolas@gmail.com>
2025-07-31 12:34:12 +0200

    drafting better script

--- scripts/show_rall.py
+++ scripts/show_rall.py
@@ -3,9 +3,11 @@
 # Author:  mikolas
 # Created on:  Thu Jul 3 12:40:15 PM UTC 2025
 # Copyright (C) 2025, Mikolas Janota
-from rall import load_dictionary
+import closure
+from rall2 import load_dictionary
 
 dat = load_dictionary("results.pkl")
+print("# loaded")
 sat = 0
 unsat = 0
 for k in dat:
@@ -20,3 +22,13 @@ print("total", len(dat))
 print("sat", sat)
 print("unsat", unsat)
 print("unknown", len(dat) - sat - unsat)
+impl = set()
+nimpl = set()
+for x, y in dat:
+    val = dat[(x, y)]
+    if val is False:
+        impl.add((x, y))
+    if val is True:
+        nimpl.add((x, y))
+new_impl, new_nimpl = closure.close_implications(impl, nimpl)
+print(len(new_impl), len(new_nimpl))
