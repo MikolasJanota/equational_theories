5fea9f71c5380a0ece1cdafe497c0cdec711599e
mikolas <mikolas@gmail.com>
2025-07-31 16:03:54 +0200

    update

--- scripts/show_rall.py
+++ scripts/show_rall.py
@@ -5,10 +5,11 @@
 # Copyright (C) 2025, Mikolas Janota
 import argparse
 
-from rall2 import load_dictionary
+from run_all import Res, ResultInfo, Results, SolverCfg, load_results
 
 
 def show_files():
+    """Show stats about res files."""
     parser = argparse.ArgumentParser()
     parser.add_argument("pkl_files", nargs="*", default=["results.pkl"])
     args = parser.parse_args()
@@ -17,22 +18,24 @@ def show_files():
 
 
 def show(pkl_file):
-    dat = load_dictionary(pkl_file)
-    sat = 0
-    unsat = 0
+    """Show stats about res file."""
+    results = load_results(pkl_file)
+    dat = results.values
+    true_eqs = 0
+    false_eqs = 0
     for k in dat:
-        val = dat[k]
-        if val is None:
-            continue
-        if val is True:
-            sat += 1
-        if val is False:
-            unsat += 1
+        # print(k, dat[k])
+        ri: ResultInfo = dat[k]
+        val: Res = ri.value
+        if val == Res.IMPL_FALSE:
+            true_eqs += 1
+        if val == Res.IMPL_TRUE:
+            false_eqs += 1
     print("===", pkl_file)
     print(f"total: {len(dat):,}")
-    print(f"sat: {sat:,}")
-    print(f"unsat: {unsat:,}")
-    print(f"unknown: {(len(dat) - sat - unsat):,}")
+    print(f"trues: {true_eqs:,}")
+    print(f"falses: {false_eqs:,}")
+    print(f"unknown: {(len(dat) - true_eqs - false_eqs):,}")
 
 
 if __name__ == "__main__":
